cf59c660aca6724afb8aa8fb6f7059c359865e2a
Kitty\cl <jimhk256@gmail.com>
2026-03-18 10:07:56 +0800

    add live update

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,15 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.test.vue',
   appName: 'vueappflow',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    LiveUpdates: {
+      appId: 'e68fc88c',
+      channel: 'Production',
+      autoUpdateMethod: 'background',
+      maxVersions: 2,
+    },
+  },
 };
 
 export default config;
